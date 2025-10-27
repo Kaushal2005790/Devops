0eef5147c3a59d4c24ba034ca8bda66a8dba2c97
Harshwardhan Kasliwal <kasliwalharsh1008@gmail.com>
2025-10-27 16:28:55 +0530

    Add build script for Jenkins and GitHub integration

---
+++ build.bat
@@ -0,0 +1,10 @@
+echo ===========================================
+echo Building project from Jenkins + GitHub
+echo ===========================================
+echo Compiling code...
+javac Hello.java
+echo Running code...
+java Hello
+echo ===========================================
+echo Build Finished Successfully!
+
